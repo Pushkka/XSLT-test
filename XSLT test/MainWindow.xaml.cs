5d31940ff39bdb71da8cb9504765807acc1b49d2
Pushka <76700084+Pushkka@users.noreply.github.com>
2026-04-18 13:14:24 +0300

    финальные изменения

--- XSLT test/MainWindow.xaml.cs
+++ XSLT test/MainWindow.xaml.cs
@@ -6,9 +6,9 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Input;
 using System.Xml.Linq;
 using Microsoft.Win32;
-using net.sf.saxon.functions;
 using Saxon.Api;
 
 namespace XSLT_test
@@ -29,17 +29,53 @@ namespace XSLT_test
         december
     }
 
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
+
+        #region Private
+
+        private string _openedFile { get; set; }
         private ObservableCollection<EmployeeGroup> _employeeGroups;
+
+        #endregion
+
+        #region Public
+
+        public string OpenedFile
+        {
+            get => _openedFile;
+            set { _openedFile = value; OnPropertyChanged(nameof(OpenedFile)); }
+        }
+        public ICommand AddEmployeeCommand { get; }
+        public ICommand AddSalaryCommand { get; }
+        public ICommand DeleteEmployeeCommand { get; }
+        public ICommand DeleteSalaryCommand { get; }
+        public ICommand SaveCommand { get; }
+
+        #endregion
+
+        #region Constructor
 
         public MainWindow()
         {
             InitializeComponent();
             _employeeGroups = new ObservableCollection<EmployeeGroup>();
             EmployeesTreeView.ItemsSource = _employeeGroups;
+
+            AddEmployeeCommand = new RelayCommand(AddEmployee);
+            AddSalaryCommand = new RelayCommand(AddSalary);
+            DeleteEmployeeCommand = new RelayCommand(DeleteEmployee);
+            DeleteSalaryCommand = new RelayCommand(DeleteSalary);
+            SaveCommand = new RelayCommand(SaveExecute);
+
+            DataContext = this;
         }
 
+        #endregion
+
+        #region Private Methods
+
+        //Выбор файла.
         private void FileSelector_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog
@@ -51,11 +87,13 @@ namespace XSLT_test
 
             if (openFileDialog.ShowDialog() == true)
             {
-                string inputXml = openFileDialog.FileName;
-                LoadPayItems(inputXml);
+                OpenedFile = openFileDialog.FileName;
+                LoadPayItems(OpenedFile);
             }
         }
 
+
+        //Загрузка текущих элементов.
         private void LoadPayItems(string filePath)
         {
             _employeeGroups.Clear();
@@ -78,7 +116,7 @@ namespace XSLT_test
 
                 foreach (var item in group)
                 {
-                    double.TryParse(item.Attribute("amount")?.Value.Replace(".",","), out double amount);
+                    double.TryParse(item.Attribute("amount")?.Value.Replace(".", ","), out double amount);
                     Enum.TryParse<Month>(item.Attribute("mount")?.Value, true, out Month month);
                     employeeGroup.Salaries.Add(new SalaryRecord
                     {
@@ -91,8 +129,104 @@ namespace XSLT_test
             }
         }
 
+
+
+        private void AddEmployee(object parameter)
+        {
+            _employeeGroups.Add(new EmployeeGroup
+            {
+                Name = "Новый",
+                Surname = "Сотрудник"
+            });
+        }
+
+        private void DeleteEmployee(object parameter)
+        {
+            if (parameter is EmployeeGroup group)
+            {
+                _employeeGroups.Remove(group);
+            }
+        }
+
+        private void AddSalary(object parameter)
+        {
+            if (parameter is EmployeeGroup group)
+            {
+                group.Salaries.Add(new SalaryRecord { Amount = 0, Month = Month.january });
+            }
+        }
+
+        private void DeleteSalary(object parameter)
+        {
+            if (parameter is SalaryRecord record)
+            {
+                var group = _employeeGroups.FirstOrDefault(g => g.Salaries.Contains(record));
+                group?.Salaries.Remove(record);
+            }
+        }
+
+        private void SaveExecute(object parameter)
+        {
+            if (_employeeGroups.Count == 0 || string.IsNullOrEmpty(OpenedFile)) return;
+
+            try
+            {
+                SaveCurrentDataToFile(OpenedFile);
+                
+                string xsltPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Transform.xslt");
+                string employeesXml = Path.Combine(Path.GetDirectoryName(OpenedFile), "Employees.xml");
+
+                RunXsltTransformation(OpenedFile, xsltPath, employeesXml);
+                AddTotalSalaryToEmployees(employeesXml);
+                AddTotalAmountToPay(OpenedFile);
+
+                MessageBox.Show($"Данные сохранены и преобразованы!\nФайл: {employeesXml}", "Успех", MessageBoxButton.OK, MessageBoxImage.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+
+
+        //Пересохраняет файл.
+        private void SaveCurrentDataToFile(string filePath)
+        {
+            Console.WriteLine("SaveCurrentDataToFile");
+
+            XDocument doc = XDocument.Load(filePath);
+            var payElement = doc.Element("Pay");
+
+            if (payElement != null)
+            {
+                var existingItems = payElement.Descendants("item").ToList();
+                foreach (var item in existingItems)
+                {
+                    item.Remove();
+                }
+
+                foreach (var group in _employeeGroups)
+                {
+                    foreach (var salary in group.Salaries)
+                    {
+                        payElement.Add(new XElement("item",
+                            new XAttribute("name", group.Name),
+                            new XAttribute("surname", group.Surname),
+                            new XAttribute("mount", salary.Month),
+                            new XAttribute("amount", salary.Amount.ToString("F2"))
+                        ));
+                    }
+                }
+
+                doc.Save(filePath);
+            }
+        }
+
+        //Запускает преобразование.
         private void RunXsltTransformation(string inputXml, string xsltPath, string outputXml)
         {
+            Console.WriteLine("RunXsltTransformation");
             Processor processor = new Processor();
             XsltCompiler compiler = processor.NewXsltCompiler();
             XsltExecutable executable = compiler.Compile(new Uri(xsltPath));
@@ -109,21 +243,25 @@ namespace XSLT_test
             transformer.Run(serializer);
         }
 
+        //Дописывает в элемент Employee атрибут, который отражает сумму всех amount/@salary этого элемента
         private void AddTotalSalaryToEmployees(string filePath)
         {
+            Console.WriteLine("AddTotalSalaryToEmployees");
             XDocument doc = XDocument.Load(filePath);
             foreach (var employee in doc.Descendants("Employee"))
             {
                 double sum = employee.Elements("salary")
-                    .Sum(s => double.Parse(s.Attribute("amount")?.Value.Replace(',', '.') ?? "0", CultureInfo.InvariantCulture));
+                    .Sum(s => double.Parse(s.Attribute("amount")?.Value ?? "0"));
                 
-                employee.SetAttributeValue("totalSalary", sum.ToString("F2", CultureInfo.InvariantCulture));
+                employee.SetAttributeValue("totalSalary", sum.ToString("F2"));
             }
             doc.Save(filePath);
         }
 
+        //Дописывает атрибут, который отражает сумму всех amount.
         private void AddTotalAmountToPay(string filePath)
         {
+            Console.WriteLine("AddTotalAmountToPay");
             XDocument doc = XDocument.Load(filePath);
             var payElement = doc.Element("Pay");
             if (payElement != null)
@@ -133,18 +271,25 @@ namespace XSLT_test
                 {
                     if (item.Attribute("amount") != null)
                     {
-                        totalSum += double.Parse(item.Attribute("amount").Value.Replace(',', '.'), CultureInfo.InvariantCulture);
+                        totalSum += double.Parse(item.Attribute("amount").Value);
                     }
                 }
-                payElement.SetAttributeValue("totalAmount", totalSum.ToString("F2", CultureInfo.InvariantCulture));
+                payElement.SetAttributeValue("totalAmount", totalSum.ToString("F2"));
                 doc.Save(filePath);
             }
         }
 
-        private void Save_Click(object sender, RoutedEventArgs e)
+        #endregion
+
+        #region INotify
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
         {
-            if (_employeeGroups.Count == 0) return;
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #endregion
     }
 
     public class EmployeeGroup : INotifyPropertyChanged
